9393c447aaf623f8080c0f0f95650a016be62171
Kennedy Sena <kennedysena.dev@gmail.com>
2024-09-08 14:28:55 -0400

    feat: desennvolvendo contador de tempo com  HTML, CSS e JAVASCRIPT ES6

---
+++ src/FocusTimer/state.js
@@ -0,0 +1,9 @@
+export default {
+  minutes: 25,
+  seconds: 0,
+  isRunning: false,
+  isMute: true,
+  countdownId: null,
+};
+
+// Estado inicial da minha aplicação. toda vez que eu mudar o e estado vai refletir no HTML
